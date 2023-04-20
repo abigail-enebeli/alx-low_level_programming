2cb813f5fa1a9435feb915d9aeb9363984ec1f73
abigail-enebeli <abigailenebili@gmail.com>
2023-04-20 16:07:04 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*script takes no parameters hence, void
+*main is the entry point
 *
-*script returns 0 after printing the function
+*script always 0 after printing the function
 */
 int main(void)
-{
-puts ("\"Programming is like building a multilingual puzzle");
-return(0);
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
