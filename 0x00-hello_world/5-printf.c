d4d73e123879eaeaf7c5226a8ad40d5ba5d998ba
abigail-enebeli <abigailenebili@gmail.com>
2023-04-20 16:15:02 -0700

    C program that printsgiven statement

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+* main - the entry point
+* Return: always 0 after printing the function
+*/
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
