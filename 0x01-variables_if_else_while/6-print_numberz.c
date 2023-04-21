80ac35c2be624bf3abfdd3a4376646951bba276c
abigail-enebeli <abigailenebili@gmail.com>
2023-04-21 10:31:22 -0700

    script prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main -scripts Print single numbers of base 10.
+* Return: 0
+*/
+
+int main(void)
+{
+int num;
+
+for (num = 60 ; num < 70 ; num++)
+{
+putchar(num);
+}
+putchar(10);
+return (0);
+}
